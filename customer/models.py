a18c8e6316903ed6b5b12286c326316a8e973b2d
RushikeshPawar123 <rushikeshp302@gmail.com>
2025-03-19 16:06:37 +0530

    customer modal

--- customer/models.py
+++ customer/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 class Customer(models.Model):
     customer_id=models.AutoField(primary_key=True)
-    customer_name=models.CharField(max_length=255)
-    customer_status=models.CharField(max_length=255)
-class Meta:
+    customer_name=models.CharField(max_length=255,null=True)
+    customer_mobile=models.IntegerField(null=True)
+    customer_email=models.CharField(max_length=255,null=True)
+    customer_gstno=models.IntegerField(null=True)
+    9
+    customer_panno=models.IntegerField(null=True)
+    customer_openingbal=models.IntegerField(null=True)
+    customer_grouptype=models.CharField(max_length=255,null=True)
+    customer_BillingAddress=models.CharField(max_length=255,null=True)
+    customer_ShippingAddress=models.CharField(max_length=255,null=True)
+    customer_City=models.CharField(max_length=255,null=True)
+    customer_CreditLimit=models.IntegerField(null=True)
+    customer_CreditPeriod=models.IntegerField(null=True) 
+    class Meta:
         db_table = "tbl_customer"
 
 
