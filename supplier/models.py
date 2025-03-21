645ef5232c346dda67aea3316da6eb9256fefb2f
vishal2331 <vishalsalve8530@gmail.com>
2025-03-21 11:45:54 +0530

    changes module

--- supplier/models.py
+++ supplier/models.py
@@ -2,18 +2,17 @@ from django.db import models
 class Supplier(models.Model):
     supplier_id=models.AutoField(primary_key=True)
     supplier_name=models.CharField(max_length=255,null=True)
-    supplier_mobile=models.IntegerField(null=True)
+    supplier_mobile=models.IntegerField(null=True)          
     supplier_email=models.CharField(max_length=255,null=True)
     supplier_gstno=models.IntegerField(null=True)
-    
-    supplier_panno=models.IntegerField()
-    supplier_openingbal=models.IntegerField()
-    supplier_grouptype=models.CharField(max_length=255)
+    supplier_panno=models.IntegerField(null=True)
+    supplier_openingbal=models.IntegerField(null=True)
+    supplier_grouptype=models.CharField(max_length=255,null=True)
     supplier_BillingAddress=models.CharField(max_length=255,null=True)
     supplier_ShippingAddress=models.CharField(max_length=255,null=True)
     supplier_City=models.CharField(max_length=255,null=True)
-    supplier_CreditLimit=models.IntegerField()
-    supplier_CreditPeriod=models.IntegerField() 
+    supplier_CreditLimit=models.IntegerField(null=True)
+    # supplier_CreditPeriod=models.IntegerField() 
     class Meta:
         db_table = "tbl_supplier"
 
